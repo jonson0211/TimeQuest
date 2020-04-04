6ddcbdabf452bd5f61dae650dad795eb250fc387
adamwong884 <a.k.wong@student.unsw.edu.au>
2020-04-04 20:02:05 +1100

    ADDED QUESTION PAGE XML

---
+++ app/src/main/java/com/example/timequest/ui/question/QuestionPage.java
@@ -0,0 +1,19 @@
+package com.example.timequest.ui.question;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.example.timequest.R;
+
+public class QuestionPage extends AppCompatActivity {
+
+
+}
+
