89e74eb4234a51c2554a48c178c8e3f36b30e476
lienzhu <56902061+lienzhu@users.noreply.github.com>
2020-04-13 15:39:02 +1000

    Customise Profile code skeleton

--- app/src/main/java/com/example/timequest/CustomiseActivity.java
+++ app/src/main/java/com/example/timequest/CustomiseActivity.java
@@ -3,10 +3,11 @@ package com.example.timequest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class CustomiseActivity extends AppCompatActivity {
+public class CustomiseActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView ivCharacter;
     private ImageView ivHead;
@@ -33,13 +34,75 @@ public class CustomiseActivity extends AppCompatActivity {
         ivItem2 = findViewById(R.id.ivItem2);
 
         headNextButton = findViewById(R.id.headNextButton);
+        headNextButton.setOnClickListener(this);
         headBackButton = findViewById(R.id.headBackButton);
+        headBackButton.setOnClickListener(this);
         item1NextButton = findViewById(R.id.item1NextButton);
+        item1NextButton.setOnClickListener(this);
         item1BackButton = findViewById(R.id.item1BackButton);
+        item1BackButton.setOnClickListener(this);
         item2NextButton = findViewById(R.id.item2NextButton);
+        item2NextButton.setOnClickListener(this);
         item2BackButton = findViewById(R.id.item2BackButton);
+        item2BackButton.setOnClickListener(this);
+
         saveButton = findViewById(R.id.saveButton);
 
+    }
+
+    int headCounter = 1;
+    int item1Counter = 1;
+    int item2Counter = 1;
+
+    @Override
+    public void onClick(View v) {
+
+        int headid = getResources().getIdentifier("item" + headCounter,"drawable", getPackageName());
+        int item1id = getResources().getIdentifier("item" + headCounter,"drawable", getPackageName());
+        int item2id = getResources().getIdentifier("item" + headCounter,"drawable", getPackageName());
+
+        switch (v.getId()) {
+
+            case R.id.headNextButton:
+
+                if(headCounter < 6) {
+                    headCounter++;
+                }else if
+               (headCounter == 6){
+                    headCounter = 1;
+            }
+                ivHead.setImageResource(headid);
+                break;
+
+            case R.id.headBackButton:
+
+                if(headCounter > 1) {
+                    headCounter--;
+                }else if(headCounter == 1) {
+                    headCounter= 6;
+        }
+                ivHead.setImageResource(headid);
+                break;
+
+            case R.id.item1NextButton:
+                // do your code
+                break;
+
+            case R.id.item1BackButton:
+                // do your code
+                break;
+
+            case R.id.item2NextButton:
+                // do your code
+                break;
+
+            case R.id.item2BackButton:
+                // do your code
+                break;
+
+            default:
+                break;
+        }
 
     }
 }
