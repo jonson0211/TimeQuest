abe4f374e6193d33d8c3387d9a1cd6a67178685b
Jonson <jonson0211@hotmail.com>
2020-04-15 01:35:36 +1000

    DAOs

--- app/src/main/java/com/example/timequest/DAOs/HandItemsDAO.java
+++ app/src/main/java/com/example/timequest/DAOs/HandItemsDAO.java
@@ -1,10 +1,14 @@
 package com.example.timequest.DAOs;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 @Dao
-public interface HandItemsDAO {
-
+public interface HandItemsDAO
+{
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    public void insertHandItem(String handItem);
 
 }
