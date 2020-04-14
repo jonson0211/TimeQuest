72369455ce0857cba53092d748263397afe2eeab
Jonson <jonson0211@hotmail.com>
2020-04-14 15:57:17 +1000

    Yes

--- app/src/main/java/com/example/timequest/DAOs/HandItemsDAO.java
+++ app/src/main/java/com/example/timequest/DAOs/HandItemsDAO.java
@@ -1,7 +1,10 @@
 package com.example.timequest.DAOs;
 
 import androidx.room.Dao;
+import androidx.room.Query;
 
 @Dao
 public interface HandItemsDAO {
+
+
 }
--- app/src/main/java/com/example/timequest/DAOs/UserDAO.java
+++ app/src/main/java/com/example/timequest/DAOs/UserDAO.java
@@ -1,7 +1,26 @@
 package com.example.timequest.DAOs;
 
 import androidx.room.Dao;
+import androidx.room.Query;
 
 @Dao
 public interface UserDAO {
+
+    @Query("UPDATE user SET user_hand_item = :handItemName")
+    void changeHandItem(String handItemName);
+
+    @Query("UPDATE user SET user_head_item = :headItemName")
+    void changeHeadItem(String headItemName);
+
+    @Query("UPDATE user SET user_body_item = :bodyItemName")
+    void changeBodyItem(String bodyItemName);
+
+    @Query("UPDATE user SET progress = :newProgress")
+    void changeProgress(double newProgress);
+
+
+    @Query("UPDATE user SET accuracy = :newAccuracy")
+    void changeAccuracy(double newAccuracy);
+
+
 }
